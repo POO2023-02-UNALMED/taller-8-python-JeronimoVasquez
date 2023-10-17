8065ecd6ca44ef2dd0057dff40153d73f8c726a0
JeronimoVasquez <134629634+JeronimoVasquez@users.noreply.github.com>
2023-10-16 22:24:22 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -28,5 +28,4 @@ class Futbolista(Persona, Deportista):
         return self._piernaHabil
     
     def __str__(self):
-        return f'Mi nombre es {self.getNombre()} soy profesional\
-            en el deporte {self.getDeporte()}. Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte'
+        return f'Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte'
